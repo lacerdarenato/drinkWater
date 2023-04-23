04af8b37cdd56387cd1c3c1ad3a8a3c9daa9a037
Renato Lacerda <lacerdarenato00@gmail.com>
2023-04-23 18:04:55 -0300

    feat: create route to get_person_by_id

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from model import person, dailyConsumption
+import datetime
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
 app = Flask(__name__)
@@ -24,12 +24,44 @@ def home():
 @app.route('/person', methods=['POST'])
 def create_person():
     request_data = request.get_json()
-    new_person = person.PersonModel(request_data['name'], request_data['weight'])
+    new_person = person.PersonModel(
+        request_data['name'], request_data['weight'])
     new_person.save_to_db()
     result = person.PersonModel.find_one_by_id(new_person.id)
-    
+
     return jsonify(result.json())
-    
+
+
+@app.route('/person/<int:id>/', methods=['GET'])
+def get_person_by_id(id):
+    result = person.PersonModel.find_one_by_id(id)
+    if result:
+        return result.json()
+    return {'message': f'Pessoa com id: {id} nao existe'}, 404
+
+
+@app.route('/person/<int:id>/consumption', methods=['POST'])
+def create_consumption_for_person(id):
+    request_data = request.get_json()
+
+    if not 'date' in request_data:
+        request_data['date'] = datetime.datetime.now()
+
+    person_searched = person.PersonModel.find_one_by_id(id)
+    if person_searched:
+        new_consumption = dailyConsumption.DailyConsumptionModel(
+            date=request_data['date'],
+            target=request_data['target'],
+            remaining=request_data['remaining'],
+            consumption=request_data['consumption'],
+            percentage=request_data['percentage'],
+            is_goal=request_data['is_goal'],
+            person_id=person_searched.id)
+        
+        new_consumption.save_to_db()
+        return new_consumption.json()
+
+    return {'message': f'Pessoa com id: {id} nao encontrada'}, 404
 
 
 if __name__ == '__main__':
