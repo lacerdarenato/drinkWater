887d19026a118b43a2a95897db1223892ea0ea37
Renato Lacerda <lacerdarenato00@gmail.com>
2023-04-23 15:01:15 -0300

    feat: setup sqlite on app

--- api/app.py
+++ api/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from data import alchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print(f'{os.getenv("ACCESS_TOKEN")}')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXEPTION'] = True
+
+@app.before_first_request
+def create_tables():
+  alchemy.create_all()
 
 @app.route('/', methods=["GET"])
 def home():
